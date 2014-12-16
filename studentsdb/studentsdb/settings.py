d3de2cc5decbbf621fa46d5fe7a6a87bf9d9f5ed
Alexander Tyapkin <atyapkin20@gmail.com>
2014-12-16 22:08:55 +0200

    Created fixtures and migrated on mysql

--- studentsdb/studentsdb/settings.py
+++ studentsdb/studentsdb/settings.py
@@ -50,10 +50,20 @@ WSGI_APPLICATION = 'studentsdb.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/1.7/ref/settings/#databases
 
-DATABASES = {
+"""DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.sqlite3',
         'NAME': os.path.join(BASE_DIR, '..', 'db.sqlite3'),
+    }
+}"""
+
+DATABASES = {
+    'default': {
+        'ENGINE': 'django.db.backends.mysql',
+        'NAME': 'students_db',
+        'HOST': 'localhost',
+        'USER': 'students_db_user',
+        'PASSWORD': '1234',
     }
 }
 
