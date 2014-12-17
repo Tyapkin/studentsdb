a688e357eb903beb561922b62232a6042eb5c35b
Alexander Tyapkin <atyapkin20@gmail.com>
2014-12-17 19:47:10 +0200

    Implemented group view

--- studentsdb/students/views/groups.py
+++ studentsdb/students/views/groups.py
@@ -1,11 +1,37 @@
 # -*- coding: utf-8 -*-
 from django.shortcuts import render
 from django.http import HttpResponse
+from django.core.paginator import Paginator, EmptyPage, PageNotAnInteger
+
+from ..models import Group
 
 
 # Views for groups
 def groups_list(request):
-    return render(request, 'students/groups_list.html', {})
+    groups = Group.objects.all()
+
+    # try to order groups list
+    order_by = request.GET.get('order_by', '')
+
+    if order_by in ('title', 'leader'):
+        groups = groups.order_by(order_by)
+
+        if request.GET.get('reverse', '') == '1':
+            groups = groups.reverse()
+
+    paginator = Paginator(groups, 3)
+    page = request.GET.get('page')
+
+    try:
+        groups = paginator.page(page)
+    except PageNotAnInteger:
+        # if page is not an integer, deliver first page
+        groups = paginator.page(1)
+    except EmptyPage:
+        # if page is out of range (e.g. 9999), deliver last page of results
+        groups = paginator.page(paginator.num_pages)
+
+    return render(request, 'students/groups_list.html', {'groups': groups})
 
 
 def groups_add(request):
