f75c61e865e3a550043ce07e585fdc61b82f8fef
Alexander Tyapkin <atyapkin20@gmail.com>
2014-12-17 15:23:50 +0200

    Now configure the database in a separate file

---
+++ studentsdb/studentsdb/database_settings.py
@@ -0,0 +1,9 @@
+DATABASES = {
+    'default': {
+        'ENGINE': 'django.db.backends.mysql',
+        'NAME': 'students_db',
+        'HOST': 'localhost',
+        'USER': 'students_db_user',
+        'PASSWORD': '1234',
+    }
+}
--- studentsdb/studentsdb/settings.py
+++ studentsdb/studentsdb/settings.py
@@ -50,22 +50,15 @@ WSGI_APPLICATION = 'studentsdb.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/1.7/ref/settings/#databases
 
-"""DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, '..', 'db.sqlite3'),
+try:
+    from database_settings import DATABASES
+except ImportError:
+    DATABASES = {
+        'default': {
+            'ENGINE': 'django.db.backends.sqlite3',
+            'NAME': os.path.join(BASE_DIR, '..', 'db.sqlite3'),
+        }
     }
-}"""
-
-DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.mysql',
-        'NAME': 'students_db',
-        'HOST': 'localhost',
-        'USER': 'students_db_user',
-        'PASSWORD': '1234',
-    }
-}
 
 TEMPLATE_CONTEXT_PROCESSORS = global_settings.TEMPLATE_CONTEXT_PROCESSORS + (
     'django.core.context_processors.request',
