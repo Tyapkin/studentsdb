1548501f3b09cd304769057b16db83fe9df78d34
Alexander Tyapkin <atyapkin20@gmail.com>
2014-12-18 22:08:30 +0200

    Implemented model for exams

--- studentsdb/students/migrations/0001_initial.py
+++ studentsdb/students/migrations/0001_initial.py
@@ -2,6 +2,7 @@
 from __future__ import unicode_literals
 
 from django.db import models, migrations
+import django.db.models.deletion
 
 
 class Migration(migrations.Migration):
@@ -10,6 +11,34 @@ class Migration(migrations.Migration):
     ]
 
     operations = [
+        migrations.CreateModel(
+            name='Exam',
+            fields=[
+                ('id', models.AutoField(verbose_name='ID', serialize=False, auto_created=True, primary_key=True)),
+                ('exam_name', models.CharField(max_length=40, verbose_name='\u041d\u0430\u0437\u0432\u0430 \u0456\u0441\u043f\u0438\u0442\u0443')),
+                ('date_exam', models.DateTimeField(verbose_name='\u0414\u0430\u0442\u0430 \u043f\u0440\u043e\u0432\u0435\u0434\u0435\u043d\u043d\u044f \u0456\u0441\u043f\u0438\u0442\u0443')),
+                ('teacher', models.CharField(max_length=40, verbose_name='\u0412\u0438\u043a\u043b\u0430\u0434\u0430\u0447')),
+                ('auditorium', models.CharField(default='a1', max_length=2, verbose_name='\u0410\u0443\u0434\u0438\u0442\u043e\u0440\u0456\u044f', choices=[(b'a1', b'101'), (b'a2', b'102'), (b'a3', b'103'), (b'a4', b'201'), (b'a5', b'202'), (b'a6', b'203')])),
+            ],
+            options={
+                'verbose_name': '\u0406\u0441\u043f\u0438\u0442',
+                'verbose_name_plural': '\u0406\u0441\u043f\u0438\u0442\u0438',
+            },
+            bases=(models.Model,),
+        ),
+        migrations.CreateModel(
+            name='Group',
+            fields=[
+                ('id', models.AutoField(verbose_name='ID', serialize=False, auto_created=True, primary_key=True)),
+                ('title', models.CharField(max_length=20, verbose_name='\u041d\u0430\u0437\u0432\u0430')),
+                ('notes', models.TextField(verbose_name='\u0414\u043e\u0434\u0430\u0442\u043a\u043e\u0432\u0456 \u043d\u043e\u0442\u0430\u0442\u043a\u0438', blank=True)),
+            ],
+            options={
+                'verbose_name': '\u0413\u0440\u0443\u043f\u0430',
+                'verbose_name_plural': '\u0413\u0440\u0443\u043f\u0438',
+            },
+            bases=(models.Model,),
+        ),
         migrations.CreateModel(
             name='Student',
             fields=[
@@ -21,9 +50,24 @@ class Migration(migrations.Migration):
                 ('photo', models.ImageField(upload_to=b'', null=True, verbose_name='\u0424\u043e\u0442\u043e', blank=True)),
                 ('ticket', models.CharField(max_length=20, verbose_name='\u0411\u0456\u043b\u0435\u0442')),
                 ('notes', models.TextField(verbose_name='\u0414\u043e\u0434\u0430\u0442\u043a\u043e\u0432\u0456 \u043d\u043e\u0442\u0430\u0442\u043a\u0438', blank=True)),
+                ('student_group', models.ForeignKey(on_delete=django.db.models.deletion.PROTECT, verbose_name='\u0413\u0440\u0443\u043f\u0430', to='students.Group', null=True)),
             ],
             options={
+                'verbose_name': '\u0421\u0442\u0443\u0434\u0435\u043d\u0442',
+                'verbose_name_plural': '\u0421\u0442\u0443\u0434\u0435\u043d\u0442\u0438',
             },
             bases=(models.Model,),
+        ),
+        migrations.AddField(
+            model_name='group',
+            name='leader',
+            field=models.OneToOneField(null=True, on_delete=django.db.models.deletion.SET_NULL, blank=True, to='students.Student', verbose_name='\u0421\u0442\u0430\u0440\u043e\u0441\u0442\u0430'),
+            preserve_default=True,
+        ),
+        migrations.AddField(
+            model_name='exam',
+            name='exam_group',
+            field=models.ForeignKey(on_delete=django.db.models.deletion.SET_NULL, verbose_name='\u0413\u0440\u0443\u043f\u0430', blank=True, to='students.Group', null=True),
+            preserve_default=True,
         ),
     ]
---
+++ studentsdb/students/models/exams.py
@@ -0,0 +1,38 @@
+# -*- coding: utf-8 -*-
+from django.db import models
+
+
+class Exam(models.Model):
+    """Exams model"""
+
+    AUDITORIUM_LIST = (
+        ('a1', '101'), ('a2', '102'), ('a3', '103'),
+        ('a4', '201'), ('a5', '202'), ('a6', '203'),
+    )
+
+    exam_name = models.CharField(max_length=40, blank=False,
+                                 verbose_name=u'Назва іспиту')
+
+    date_exam = models.DateTimeField(blank=False,
+                                     verbose_name=u'Дата проведення іспиту')
+
+    teacher = models.CharField(max_length=40, blank=False,
+                               verbose_name=u'Викладач')
+
+    exam_group = models.ForeignKey('Group', blank=True, null=True,
+                                   on_delete=models.SET_NULL,
+                                   verbose_name=u'Група')
+
+    auditorium = models.CharField(max_length=2, choices=AUDITORIUM_LIST,
+                                  default=u'a1',
+                                  verbose_name=u'Аудиторія')
+
+    class Meta(object):
+        verbose_name = u'Іспит'
+        verbose_name_plural = u'Іспити'
+
+    def __unicode__(self):
+        if self.teacher:
+            return '%s (%s)' % (self.exam_name, self.teacher)
+        else:
+            return '%s' % self.exam_name
