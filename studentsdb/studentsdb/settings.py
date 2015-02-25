829905af60f2e45f21da0e9a7aa7f54bc66e0326
Alexander Tyapkin <atyapkin20@gmail.com>
2015-02-25 11:31:10 +0200

    Server side code for group selector - Done

--- studentsdb/studentsdb/settings.py
+++ studentsdb/studentsdb/settings.py
@@ -65,6 +65,7 @@ except ImportError:
 TEMPLATE_CONTEXT_PROCESSORS = global_settings.TEMPLATE_CONTEXT_PROCESSORS + (
     'django.core.context_processors.request',
     'studentsdb.context_processors.students_proc',
+    'students.context_processors.groups_processor',
 )
 
 # Internationalization
