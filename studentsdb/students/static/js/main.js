6491e68b947a1426b570ee2e0abf01009cc36793
Alexander Tyapkin <atyapkin20@gmail.com>
2015-02-25 21:11:11 +0200

    Widget datepicker - done

--- studentsdb/students/static/js/main.js
+++ studentsdb/students/static/js/main.js
@@ -49,7 +49,16 @@ function initGroupSelector() {
     });
 }
 
+function initDateFields() {
+    $('input.dateinput, input.datetimeinput').datetimepicker({
+        'format': 'YYYY-MM-DD'
+    }).on('dp.hide', function(event) {
+        $(this).blur();
+    });
+}
+
 $(document).ready(function() {
     initJournal();
     initGroupSelector();
+    initDateFields();
 });
