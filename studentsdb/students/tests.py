05109b9f72aa8402e398ebe6885142974f97ec03
Alexander Tyapkin <atyapkin20@gmail.com>
2015-06-18 23:19:49 +0300

    Writing tests begin :)

--- studentsdb/students/tests.py
+++ studentsdb/students/tests.py
@@ -1,2 +1,9 @@
 # -*- coding: utf-8 -*-
 from django.test import TestCase
+from .models.students import Student
+
+
+class StudentModelTestCase(TestCase):
+
+    def test_create_student_model(self):
+        pass
